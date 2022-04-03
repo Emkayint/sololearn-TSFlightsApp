4f232ed9d4a7b9acbbe50922fcf06d1ff1fa53ad
Martin <mkmartinoes@gmail.com>
2022-04-03 19:47:25 +0300

    flight  services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FlightsService } from './flights/flights.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FlightsService],
 })
 export class AppModule {}
